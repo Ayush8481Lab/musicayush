61e971e2900f61568b70ff9c096800a1e66e6eff
Ayush8481Lab <wonder945177@gmail.com>
2026-03-31 15:54:11 +0530

    Update MiniPlayer.tsx

--- components/MiniPlayer.tsx
+++ components/MiniPlayer.tsx
@@ -1,4 +1,4 @@
- "use client";
+"use client";
 import { useEffect, useState, useRef } from "react";
 import { useAppContext } from "../context/AppContext";
 import { Play, Pause, SkipForward, SkipBack, Loader2 } from "lucide-react";

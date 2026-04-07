c84b59403273d5f697aa827d288c372958f62470
Ayush8481Lab <wonder945177@gmail.com>
2026-04-07 22:04:46 +0530

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Required dummy fetch handler to satisfy Chrome PWA install requirements
+});
